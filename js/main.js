13fb46a0c595ae31af6f5667e50cf57c0cad6c60
fungyingau <fungying@dds.nl>
2024-10-31 17:57:22 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,276 @@
+var d = document;
+
+window.onload = (function() {   
+   
+    var linkData = [
+        { text: 'Home', href: '/' },
+        { text: 'Modern', href: '/pages/modern.html' },
+        { text: 'Klassiek', href: '/pages/klassiek.html' }
+    ];
+
+    imageMapResize();
+    initHeaderNav(linkData);
+    initFooterNav(linkData); 
+    initWonderen();  
+});
+
+// console.log(imageMapResize());
+
+// standaard melding vewijderen.
+var jsDis = document.querySelector(".js-disabled");
+if (jsDis) {
+    jsDis.remove();
+    console.log("Het geselecteerde element is verwijderd:", jsDis);
+} else {
+    console.log("Geen overeenkomstend element gevonden met de klasse 'js-disabled'.");
+}
+
+/* ===================== header ======================*/
+
+function initHeaderNav(linkData) {
+
+    // return false; // WEGHALEN BIJ HET BEGIN VAN DE OPDRACHT!
+/* 	======================================================================	
+		Browser & Object sniffing
+	=========================================================================== */
+    
+    var header = d.querySelector("header");
+    if (!header) {
+        console.log("Het <header> element is niet gevonden.");
+    } else {
+        console.log("Het <header> element is gevonden:", header);
+    }
+
+    // Maak het <nav> element aan
+    var nav = d.createElement('nav');
+
+    // Maak de <nav>-<ul> elementen aan
+    var navUl = document.createElement('ul');
+
+    // Voeg de <ul> toe aan het <nav> element
+    nav.appendChild(navUl)
+
+    // Voeg het <nav> element toe aan het <header> element
+    header.appendChild(nav);   
+
+    // Maak nieuwe <li> en <a> elementen voor elke link en voeg deze toe aan de <ul>
+    for (var i = 0; i < linkData.length; i++) {
+        var linkItem = linkData[i];
+
+        // Maak een nieuw <li> element aan voor elke link
+        var li = document.createElement('li');
+
+        // Maak een nieuw <a> element aan voor elke link
+        var link = document.createElement('a');
+
+        // Stel de tekst en href van de link in op basis van linkData
+        link.textContent = linkItem.text;
+        link.href = linkItem.href;
+
+        // Voeg het <a> element toe aan het <li> element
+        li.appendChild(link);
+
+        // Voeg het <li> element toe aan het <ul> element
+        navUl.appendChild(li);
+
+        // Voeg de class "active" toe aan het <li> element op basis van de paginatitel
+        if ((linkItem.text === 'Home' && document.title === 'Wereld wonderen') ||
+            (linkItem.text === 'Modern' && document.title === 'Moderne wereld wonderen') ||
+            (linkItem.text === 'Klassiek' && document.title === 'Klassieke wereld wonderen')) {
+            li.classList.add('active'); 
+        }
+    }
+}
+
+/* ===================== pop-up ======================*/
+
+function initWonderen() {
+
+    var popupContent = d.getElementById("popupContent");
+    var popupLinks = d.getElementsByClassName("popupLink");
+    var overlay = d.getElementById('overlay');
+    console.log(overlay);
+
+    if(!d.getElementById("popupContent")) return false;
+    console.log(popupContent);
+
+	// maaak pop-up inhoud
+
+    // Maak het <div> element aan en geef een id
+	var dynamicText = d.createElement("div");
+	dynamicText.setAttribute('id', 'dynamicText');
+
+    // Maak het <h2> element aan en geef een id
+	var wonderNaam = d.createElement("h2");
+	wonderNaam.setAttribute('id', 'wonderNaam');
+
+    // Maak het <h3> element aan en geef een id
+	var wonderCategorie = d.createElement("h3");
+	wonderCategorie.setAttribute('id', 'categorie');
+	
+    // Maak het <h4> element aan en geef een id
+	var wonderLand = d.createElement('h4');
+	wonderLand.setAttribute('id', 'land');
+
+    // Maak het <div> element aan en geef een class en een id
+	var dynamicPlaatje = d.createElement('div');
+	dynamicPlaatje.setAttribute('class', 'dynamicPlaatje');
+	dynamicPlaatje.setAttribute('id', 'wonderPlaatje');
+
+    // Maak het <div> element aan en geef een class en een id
+	var dynamicP = d.createElement('div');
+	dynamicP.setAttribute('id', 'dynamicP');
+
+
+	// Maak het <img> element aan en geef een width aan
+	var dynamicPlaatjeImg = d.createElement('img');
+	dynamicPlaatjeImg.setAttribute('width', '100%');
+	
+	// Maak het <p> element aan en geef een id aan
+	var wonderOmschrijving = d.createElement('p');
+	wonderOmschrijving.setAttribute('id', 'omschrijving');
+
+	// sluitknop
+	var btn = d.createElement('button');
+	btn.id = 'close;'
+	btn.textContent = 'Sluit';
+	//console.log(btn);
+		
+	
+	// appending
+	popupContent.appendChild(dynamicText);
+	dynamicText.appendChild(wonderNaam);
+	dynamicText.appendChild(wonderCategorie);
+	dynamicText.appendChild(wonderLand);
+
+	popupContent.appendChild(dynamicPlaatje);
+	dynamicPlaatje.appendChild(dynamicPlaatjeImg);
+
+	popupContent.appendChild(dynamicP);
+	dynamicP.appendChild(wonderOmschrijving);
+
+	popupContent.appendChild(btn);
+    
+
+    /**
+    * update de popup met alle info van het wereld wonder
+    * 
+    * @param {integer} wonderNr
+    * 
+    * @return {void}
+    */
+    function updateWonder(wonderNr) {
+        var wonder = wonderen[wonderNr]; 
+        console.log(wonder);
+		wonderNaam.textContent = wonder.naam;
+		wonderCategorie.textContent = wonder.categorie;
+		wonderLand.textContent = wonder.land;
+		wonderOmschrijving.innerHTML = wonder.omschrijving;
+		dynamicPlaatjeImg.src = '../img/' + wonder.image;
+		dynamicPlaatjeImg.alt = wonder.naam;		
+    }
+
+	var popupLinks = document.getElementsByClassName("popupLink");
+		for (var i = 0; i < popupLinks.length; i++) {
+    	    popupLinks[i].addEventListener("click", function(e) {
+            e.preventDefault(); // Voorkom standaardgedrag van de link
+            var popupNaam = this.getAttribute("data-popup"); // Haal de naam van de popup op
+            openPopup(popupNaam); // Open de popup
+    	    });
+	    }
+    
+        
+	// Functie om de popup te openen en de juiste inhoud te laden
+    
+	function openPopup(popupNaam) {
+		if (!popupContent) return; // Stop als de popup niet bestaat
+		popupContent.classList.add("show"); // Voeg een klasse toe om de popup te tonen
+        overlay.classList.add('show');
+		console.log(popupNaam);
+		console.log(popupNaam.substring(5));
+        updateWonder(popupNaam.substring(5));
+	}
+
+	btn.addEventListener('click', function() {
+		// Verwijder de class .show van het pop-up en de overlay
+		popupContent.classList.remove('show');
+        overlay.classList.remove('show'); 
+	});	
+
+    overlay.addEventListener('click',function() {
+        // Verwijder de class .show van het pop-up en de overlay
+        overlay.classList.remove('show');
+        popupContent.classList.remove('show');
+    })
+}
+
+/* ===================== footer ======================*/
+
+function initFooterNav(linkData) {
+    var footer = d.querySelector('footer'); // Selecteer het footer-element
+    if (!footer) {
+        console.error("Footer niet gevonden");
+        return;
+    }
+
+    // Code om navigatie aan de footer toe te voegen
+    var nav = d.createElement('nav');
+    var navUl = d.createElement('ul');
+    nav.appendChild(navUl);
+
+    // Voeg nav toe als het eerste kind van de footer
+    var firstChild = footer.firstChild;
+    footer.insertBefore(nav, firstChild);
+    
+    for (var i = 0; i < linkData.length; i++) {
+        var linkItem = linkData[i];
+        var li = d.createElement('li');
+        var link = d.createElement('a');
+        link.textContent = linkItem.text;
+        link.href = linkItem.href;
+        li.appendChild(link);
+        navUl.appendChild(li);
+
+        // Voeg de class "active" toe aan het <li> element op basis van de paginatitel
+        if ((linkItem.text === 'Home' && document.title === 'Wereld wonderen') ||
+            (linkItem.text === 'Modern' && document.title === 'Moderne wereld wonderen') ||
+            (linkItem.text === 'Klassiek' && document.title === 'Klassieke wereld wonderen')) {
+            li.classList.add('active');
+        }
+    }
+}
+
+/* 	==================== GSAP ==================== */
+const redPins = d.querySelector('.pin_red');
+const bluePins = d.querySelector('.pin_blue');
+const mainSpan = d.querySelector('main span');
+// dropping pins
+if (redPins || bluePins) {
+    gsap.from (['.pin_red', '.pin_blue'],{
+        opacity: 0,
+        duration: 0.5,
+        y:-350, ease:'back.out(1)',
+        stagger: 0.2
+    });
+}
+
+// growing & flashing message
+if (mainSpan) {
+    gsap.from (' main span', {
+        scale: 0,
+        duration: 0.8,
+        ease: 'power3.inOut',
+        delay: 1
+    });
+    gsap.fromTo ('main span', {
+        opacity: 1},{
+            opacity: 0.2,
+            duration: 0.7,
+            ease: 'power2.out',
+            yoyo: true,
+            repeat: -1,
+            delay: 2.4
+        }
+    )
+}
+    
---
+++ js/ww_database.js
@@ -0,0 +1,163 @@
+var wonderen = [];
+var wonder = {};
+wonderen.push(wonder);
+// wonder 0 is al in de Array gezet dus alle kaarten beginnen met 1,2,3,etc
+
+// wonder 1
+wonder = {
+    naam            : 'Chichén Itzá',
+    land            : 'Mexico',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `Chichén Itzá behoort tot één van de allermooiste ruïne steden van de wereld. Het ligt in Mexico op het schiereiland Yucatán en ligt dichtbij de krater van Chicxulub. Deze krater is gemaakt door een meteoriet die hoogstwaarschijnlijk de Dinosauriërs liet uitsterven.
+    Chichén Itzá is rond 800 door de Maya’s gebouwd. El Castillo was de tempel voor hun god Kukulcan. Het is een trappenpiramide met trappen aan alle zijdes met een plateau. Achter de trappen zit het verhaal dat deze fungeren als kalender. De tempel bestaat namelijk uit 18 niveaus, omdat de Maya kalender uit 18 maanden bestond. Het totaal aantal treden is 365 wat refereert aan de aantal dagen die in een jaar zitten. En het aantal vlakken op de zijkanten van de tempel is 52. Dit zijn de aantal jaren die voor de Maya’s in een eeuw zitten.`,
+    image           : 'chichen_itza.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 2
+wonder = {
+    naam            : 'Machu Picchu',
+    land            : 'Peru',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `Op een geïsoleerde ligging, op ruim 2400 meter hoogte werd een eeuw geleden (in 1911) de Peruaanse Inca-stad Machu Picchu ontdekt. Dankzij de unieke locatie is Machu Picchu nooit eerder door Spaanse veroveraars ontdekt. Machu Picchu betekent 'Oude Top' wat hoogstwaarschijnlijk een verwijzing is naar de hoogte van de berg.
+    Het schijnt dat Machu Picchu een koninklijk buitenverblijf was, gebouwd rond het jaar 1140 met religieuze Inca doeleinden. Er waren relatief veel woningen voor edelen en slechts een paar woningen voor de bedienden. In totaal was er plek voor 750 personen. Om op deze plek te komen moesten de Inca’s meerdere dagen lopen over een erg steil pad. Een moeilijke plek om te komen dus. Tegenwoordig rijden er treinen en bussen naar Machu Picchu zodat iedereen de kans heeft om Machu Picchu, een van de zeven wereldwonderen, te zien.`,
+    image           : 'machu_picchu.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 3
+wonder = {
+    naam            : 'Cristo Redentor',
+    land            : 'Brazilië',
+    categorie       : 'mmoderne wereldwonder',
+    omschrijving    : `Cristo Redentor is het bekendste landmark van Rio de Janeiro. Het beroemde jezusbeeld kent iedereen wel. Het was de droom van een priester, Pedro Maria Boss, in 1850 om een standbeeld te laten bouwen boven op de berg Corcovado. Echter Koningin Isabel zag er weinig in. In 1921 lukte het de kerk alsnog om voldoende geld in te zamelen tijdens de Semana do Monumento, Monumentenweek. Op 12 oktober 1931 kon het beeld uiteindelijk officieel ingewijd worden door de toenmalige president Getúlio Vargas.
+    Sindsdien staat het beeld op de top van Corcovadoberg, op een hoogte van 710 meter, waar het 'waakt' over Rio de Janeiro. Het beeld werd echter niet in Brazilië gebouwd. Het beeld is in eerste instantie in Frankrijk gemaakt in blokken van zo’n 16 ton. Het bouwwerk is 38 meter hoog en weegt in totaal 1145 ton. De spanwijdte van de armen is 28 meter. Best indrukwekkend toch?!`,
+    image           : 'cristo_redentor.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 4
+wonder = {
+    naam            : 'Colosseum',
+    land            : 'Italië',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `Een van de zeven wereldwonderen dat iets dichterbij huis ligt is het beroemde Colosseum in Rome. Het Colosseum heeft een omtrek van meer dan 520 meter. Het is zo’n 50 meter hoog, 155 meter breed en 188 meter lang. Het gangenstelsel is bereikbaar door middel van 80 ingangen en bood ruimte voor ruim 50.000 toeschouwers. Iedere sociale klasse had zijn eigen zitplaats. Zo zat de keizer helemaal bovenaan. Vanuit die plek werd aanschouwt hoe er gevechten plaatsvonden tussen dieren, tussen gladiatoren en tussen mensen en dieren. Deze gevechten eindigde pas met de dood van een van de deelnemers. Allemaal ter vermaak van de mensen in de arena.`,
+    image           : 'colosseum.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 5
+wonder = {
+    naam            : 'Petra',
+    land            : 'Jordanië',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `Petra is eeuwen geleden gebouwd door de Nabateeërs. Wanneer het precies is gebouwd is onbekend, het was ongeveer in de eerste eeuw v.Chr. De Nabateeërs waren echte kunstliefhebbers en hebben hun eigen cultuur gemengd met inspiratie uit de Griekse, Romeinse en Egyptische bouwwerken. Zo bouwde zij in de rotsen een uitgehouwen stad, met een ingenieus watersysteem. Na jarenlang bouwen werd de stad een drukke handelsstad totdat een aardbeving een groot deel verwoestte. Toen de Nabateeërs de stad verlieten, kwamen de nomadische Bedoeïenen naar de stad.
+    In de 19e eeuw werd Petra ontdekt en werd het bekend als 'the lost city'. Het bekendste bouwwerk van Petra is 'the treasury', de schatkamer van de farao. Dit monument is zo’n 40 meter hoog en erg indrukwekkend om te zien. Er woont ook nog een kleine groep Bedoeïenen in Petra, die je gewoon tegen kunt komen tijdens je bezoek.`,
+    image           : 'petra.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 6
+wonder = {
+    naam            : 'Taj Mahal',
+    land            : 'India',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `'Een traan op de wang van de tijd', zo beschreef de Indiase dichter Rabindranath Tagore over de Taj Mahal, het iconische marmeren mausoleum.
+    De Taj Mahal is één van de zeven wereldwonderen die zich bevindt op de rechteroever van de Jamuna in Agra (Noord India). In 1631 overleed de grote liefde van keizer Sjah Jahan tijdens de bevalling van hun 14e kind. Hij beloofde haar op dat moment dat hij een gebouw voor haar zou maken dat representatief stond aan haar schoonheid.
+    Sjah Jahan wijdde 17 jaar, van 1631 tot 1648 aan de bouw van dit bouwwerk ter nagedachtenis van zijn vrouw, Mumtaz Mahal en als eerbetoon aan hun eeuwige liefde. Het is gemaakt van wit marmer en rood zandsteen aan de hand van de Perzische architectuur. Het gebouw is 60 meter hoog en 60 meter breed, in perfecte symmetrie.`,
+    image           : 'taj_mahal.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 7
+wonder = {
+    naam            : 'De Chinese muur',
+    land            : 'China',
+    categorie       : 'moderne wereldwonder',
+    omschrijving    : `De Chinese muur bevindt zich China waarvan een deel in de omgeving van Beijing. Het grote bouwwerk was in eerste instantie niet bedoeld om één geheel te vormen. Het is in verschillende delen gebouwd als verdedigingsmuren om de vijand op afstand te houden. Pas tijdens de Qin-dynastie (3e eeuw na Christus) werden verschillende muren tot één geheel gevormd, ter bescherming van het noorden. Deze muren werden gemaakt van materiaal dat voor handen was, denk aan aarde en stenen.<br>
+    In 1969 maakte het ruimtevaartuig Apollo 11 een rondje om de maan. De Amerikaanse astronaut Michael Collins kreeg de kans een mythe te ontkrachten: hij kon de Chinese Muur niet met het blote oog zien. Want hoewel het door grote delen van Noord-China kronkelt, is het grootste verdedigingswerk ter wereld maar 6 meter breed. Tijdens een officiële meting in 2012 werd de lengte, van Noord-Korea in het oosten tot de Gobiwoestijn in het westen, bepaald op 21.196 kilometer.`,
+    image           : 'chinese_muur.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 8
+wonder = {
+    naam            : 'Standbeeld van Zeus',
+    land            : 'Griekenland',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `Het standbeeld van Zeus in de tempel van Olympia (aan de westkust van het moderne Griekenland) is gemaakt van ivoor en goud en werd rond 435 v.Chr. gebouwd door de Griekse beeldhouwer Pheidias. Het gigantische en verbluffende witte beeld keek uit over het terrein waar de originele Olympische spelen werden gehouden, en mensen kwamen uit het hele Griekse rijk om eer te bewijzen aan Zeus en deel te nemen aan de spelen.
+    Geleerden zijn het erover eens dat het beeld meer dan 800 jaar in de tempel heeft gestaan, maar er is enige discussie over wanneer en hoe het beeld werd vernietigd. Sommige geleerden geloven dat het beeld in de vijfde eeuw bij een brand in de tempel is verwoest. Anderen geloven dat het beeld van de tempel naar een paleis in Constantinopel is vervoerd, waar het rond het jaar 462 in een brand is vernietigd.`,
+    image           : 'zeus.jpg',
+};
+wonderen.push(wonder);
+
+// wonder 9
+wonder = {
+    naam            : 'Tempel van Artemis',
+    land            : 'Turkije',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `Gebouwd rond het jaar 550 v.Chr. door de architect Cherisphron, werd de grote marmeren tempel in Efeze (gelegen in het huidige Turkije) gebouwd ter ere van Artemis, de Griekse godin van de jacht en vruchtbaarheid. De verbluffende marmeren structuur werd omringd door 127 Ionische zuilen die een hoogte van 18 meter bereikten. De gevel van de tempel was versierd met bronzen beelden die werden toegeschreven aan een aantal deskundige beeldhouwers uit die tijd en keek uit op een binnenplaats die dienst deed als marktplaats voor mensen van heinde en verre om hun goederen te verhandelen.
+    Op 21 juli 256 v.Chr., de nacht van de geboorte van Alexander de Grote, brandde een man genaamd Herostatus het hoofdgebouw tot de grond toe in de hoop zijn naam onsterfelijk te maken. Een andere tempel werd op de plaats gereconstrueerd, maar werd in het jaar 262 door de Goten verwoest.`,
+    image           : 'artemis.jpg',
+};
+wonderen.push(wonder);
+
+// kaart 10
+wonder = {
+    naam            : 'Mausoleum van Halicarnassus',
+    land            : 'Turkije',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `Het mausoleum in Hailcarnassus (gelegen in de buurt van de moderne stad Bodrum in Turkije), dat wordt bewonderd om zijn adembenemende schoonheid, werd gebouwd als het graf voor de Perzische koning Mausollos van Caria en werd voltooid rond 35 v.Chr., een paar jaar na de dood van Mausollos. Het Mausoleum, dat niet meer bestaat, was uitzonderlijk vanwege zijn enorme omvang.
+    De grafkamer en de sarcofaag, gebouwd van wit albast en versierd met goud, bevonden zich op een getrapt podium en werden omringd door Ionische zuilen. Ondanks de grootte van het Mausoleum waren de bezoekers het meest onder de indruk van de prachtige versieringen en beelden die het versierden. Zestien eeuwen na de bouw werd het mausoleum beschadigd door een aardbeving en in het begin van de 15e eeuw vernietigden kruisvaarders wat er nog over was van het mausoleum en gebruikten de bakstenen om een kasteel te bouwen dat er nog steeds staat.`,
+    image           : 'halicarnassus.jpg',
+};
+wonderen.push(wonder);
+
+// kaart 11
+wonder = {
+    naam            : 'Colossus van Rhodos',
+    land            : 'Griekenland',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `Het 110 meter hoge standbeeld van Colossus keek slechts 56 jaar uit over de oude haven van het Griekse eiland Rhodos, maar de grootte en constructie wekten zoveel ontzag bij zijn bezoekers dat het eeuwen na zijn vernietiging werd opgenomen op de lijst van oude wonderen. De mensen van Rhodos gaven beeldhouwer Chares van Lindos de opdracht om in 294 v.Chr. het gigantische bronzen beeld van hun beschermgod Helios de Zonnegod te bouwen. Helaas werd Rhodos in 238 v.Chr. getroffen door een aardbeving en viel de geliefde Colossus van de stad. Volgens historische verslagen waren bezoekers van Rhodos echter nog steeds onder de indruk van het standbeeld, zelfs toen het in puin op de grond lag.
+    In 652 vernietigde een binnenvallend Arabisch leger de gevallen Colossus, en sommige historische verslagen beweren dat het leger de stukken vervolgens verkocht aan "een Jood uit Syrië" die ze vervoerde op de ruggen van "900 kamelen".`,
+    image           : 'rhodos.jpg',
+};
+wonderen.push(wonder);
+
+// kaart 12
+wonder = {
+    naam            : 'Pharos van Alexandrië',
+    land            : 'Egypte',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `De vuurtoren van Alexandrië bevond zich op het oude eiland Pharos, net voor de kust van de Egyptische stad Alexandrië. Ontworpen door Sostratus van Cnidus in de derde eeuw v.Chr., begon de bouw van de vuurtoren pas in 305 voor v.Chr., tijdens het bewind van Ptolemaeus Soter. De vuurtoren, gemaakt van grote blokken steen, bereikte een hoogte van ongeveer 120 meter en was eeuwenlang een van de hoogste bouwwerken die door de mens zijn gemaakt. Aanvankelijk was de structuur alleen bedoeld als een oriëntatiepunt voor schepen die probeerden de haven van Alexandrië binnen te varen, maar in de eerste eeuw transformeerden de Romeinen het in een vuurtoren door ’s nachts een vuur aan te steken en overdag reflecterende spiegels te gebruiken.
+    Na 16 eeuwen schepen in veiligheid te hebben gebracht, hebben twee aardbevingen de gerespecteerde vuurtoren zwaar beschadigd. En in 1480 verwoestte de sultan van Egypte, Qaitbay, de overgebleven ruïnes en bouwde een middeleeuws fort op de plek met een deel van de originele steen van de vuurtoren.`,
+    image           : 'pharos.jpg',
+};
+wonderen.push(wonder);
+
+// kaart 13
+wonder = {
+    naam            : 'Piramide van Cheops',
+    land            : 'Egypte',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `De grote piramide van Gizeh, gebouwd rond het jaar 2560 v.Chr., is het enige wonder dat nog overeind staat van de oorspronkelijke lijst van zeven. Egyptologen geloven dat het prachtige monument, opgebouwd uit ongeveer 2 miljoen blokken steen en waarvan wordt aangenomen dat het ongeveer 20 jaar en 20.000 man nodig had om te voltooien, werd gebouwd als een tombe voor de vierde-eeuwse farao Khufu.
+    Door de geschiedenis heen hebben experts zich verwonderd over de opmerkelijke nauwkeurigheid van de constructie van de piramide. De basis, die 13 hectare beslaat, is bijna een perfect vierkant en elke hoek is bijna precies uitgelijnd met de vier punten op een kompas.`,
+    image           : 'piramide.jpg',
+};
+wonderen.push(wonder);
+
+// kaart 14
+wonder = {
+    naam            : 'De hangende tuinen van Babylon',
+    land            : 'Irak',
+    categorie       : 'klassieke wereldwonder',
+    omschrijving    : `De hangende tuinen van Babylon waren naar verluidt een middelpunt van de oude metropool Babylon (ongeveer 80 kilometer ten zuiden van het huidige Bagdad). Vreemd genoeg vermelden de historische verslagen van de Babyloniërs de wonderbaarlijke hangende tuinen niet, waardoor sommige geleerden betwijfelen of ze ooit hebben bestaan.
+    Ongeacht het ontbreken van vermeldingen,  de verhalen over de tuinen verspreidden zich in de loop van de tijd over de antieke wereld, en oude schrijvers hebben talloze beschrijvingen opgenomen.
+    Het verhaal gaat dat koning Nebudchadnezar II, die in de zevende eeuw v.Chr. regeerde, de hangende tuinen aanlegde om het berglandschap na te bootsen dat zijn vrouw gewend was van haar thuisland Medina. Ondanks het romantische beeld van tuinen die in de lucht zweefden, zijn veel experts het er nu over eens dat, als ze al bestonden, de hangende tuinen een uitgebreide reeks tuinen waren die op een heuvelachtig landschap op elkaar als terrassen waren aangelegd.`,
+    image           : 'babylon.jpg',
+};
+wonderen.push(wonder);
+
+
+
+console.log(wonderen);
